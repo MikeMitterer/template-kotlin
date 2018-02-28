5d51315649b09a6d55e19692b5a30912504c737a
Mike Mitterer <office@mikemitterer.at>
2018-02-28 16:55:11 +0100

    Logger added

--- src/test/kotlin/at/mikemitterer/template/GreeterTest.kt
+++ src/test/kotlin/at/mikemitterer/template/GreeterTest.kt
@@ -2,14 +2,19 @@ package at.mikemitterer.template
 
 import org.junit.Assert
 import org.junit.Test
+import org.slf4j.LoggerFactory
 
 /**
  * @since 28.02.18, 09:38
  */
 class GreeterTest : Assert() {
+    private val logger = LoggerFactory.getLogger(GreeterTest::class.java)
+    
     @Test
     fun testGreeter() {
         val greeter = Greeter("Mike")
+
+        logger.info(greeter.message)
         assertEquals("Hello, Mike!",greeter.message)
     }
 }
