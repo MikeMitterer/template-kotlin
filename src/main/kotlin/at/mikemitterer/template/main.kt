e13f1dd617ca07a85fd0fba008eda6c3f7b50206
Mike Mitterer <office@mikemitterer.at>
2018-02-28 09:09:00 +0100

    Initial release

---
+++ src/main/kotlin/at/mikemitterer/template/main.kt
@@ -0,0 +1,15 @@
+package at.mikemitterer.template
+
+class Greeter(private val name : String) {
+    fun greet() {
+        println("Hello, $name")
+    }
+}
+
+fun main(args : Array<String>) {
+    if(args.isNotEmpty()) {
+       Greeter(args[0]).greet()
+    } else {
+        println("Not cmdline-argument available!")
+    }
+}
